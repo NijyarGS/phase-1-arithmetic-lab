1a08dd643cd6ac0e18c167d8036c9400793176f3
Nijyar Ghazi <nijyar360@gmail.com>
2022-08-01 16:12:47 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+var num1 = 2;
+var num2 = 31;
+var multiply = num1 * num2;
+
+var random = Math.floor((Math.random()*10) +1);
+
+var num3 = 54; 
+var num4 = 5;
+var mod = num3 % num4;
+ var max = Math.max(1,2,3,4,5,6,7,8,9,10,20)
